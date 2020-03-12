17827839c7b3fe7c9652f0605ff8cf55f2e1f7df
Nikita Khokhlov <nkhokhlov11708@gmail.com>
2020-03-12 20:00:46 +0300

    init

---
+++ app/src/main/java/com/example/attendanceapp/HeadStudentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.attendanceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeadStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_head_student)
+    }
+}
---
+++ app/src/main/java/com/example/attendanceapp/TeacherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.attendanceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeacherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher)
+    }
+}
